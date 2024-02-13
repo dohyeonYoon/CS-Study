431ac27e4096af0b259d060eb55a3f6140f80a51
dohyeonYoon <xpdxpddld@jbnu.ac.kr>
2024-02-13 20:34:51 +0900

    algo: 11720 알고리즘 풀이

---
+++ Algorithm/bj-11720/11720_ydh.py
@@ -0,0 +1,7 @@
+# 데이터 갯수
+num = int(input())
+
+# 숫자형 데이터
+numbers = list(map(int, input()))
+
+print(sum(numbers))
---
+++ Algorithm/bj-16916/16916_ydh.py
@@ -0,0 +1,9 @@
+from sys import stdin
+
+S = stdin.readline()
+P = stdin.readline()
+
+if P in S:
+    print(1)
+else:
+    print(0)
